5861afb264d86de348692ba8b35842c22cea1e18
Ali Afshari <aliafshari66@gmail.com>
2020-01-09 23:31:53 +0330

    qqq

---
+++ CircleciTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CircleciTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
